0585590f4d2da201753b4265013adc9c4bd0af9a
OskarGąsiorowski <oskar@gasiorowski.ovh>
2023-01-15 18:12:40 +0100

    Revert "Added tailwind"
    This reverts commit 372983dd62771f7dc6c8657728232c774dba0ce2.

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './pages';
-import './index.css';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement,
--- apps/frontend/src/pages/start/StartPage.tsx
+++ apps/frontend/src/pages/start/StartPage.tsx
@@ -1,7 +1,3 @@
 export function StartPage() {
-    return (
-        <h1 className="text-sm font-bold underline text-green-900">
-            Hello world! 123
-        </h1>
-    );
+    return <h1>Start page</h1>;
 }
